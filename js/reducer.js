385e7868ecaa1fb5417f3c4cd0abb4193d5b422e
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-21 01:22:11 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,27 @@
-// add code snippets from README
+let state;
+
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+};
+
+const render = () => {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+};
+
+const dispatch = (action) => {
+  state = reducer(state, action);
+  render();
+};
+
+dispatch({ type: "@@INIT" });
+
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  dispatch({ type: "INCREASE_COUNT" });
+});
